55d13fecadc504851a2736ea7b903ebb7acca5ee
SkiletCool-desing <128517776+SkiletCool-desing@users.noreply.github.com>
2023-10-31 17:58:41 +0200

    react

---
+++ src/components/profile/Profile.jsx
@@ -0,0 +1,28 @@
+import './Profile.styles.css';
+export const Profile = ({ username, tag, location, avatar, stats }) => {
+  return (
+    <div className="profile">
+      <div className="description">
+        <img src={avatar} alt="User avatar" className="avatar" />
+        <p className="name">{username}</p>
+        <p className="tag">@{tag}</p>
+        <p className="location">{location}</p>
+      </div>
+
+      <ul className="stats">
+        <li className="stat-elem">
+          <span className="label">Followers</span>
+          <span className="quantity">{stats.followers}</span>
+        </li>
+        <li className="stat-elem">
+          <span className="label">Views</span>
+          <span className="quantity">{stats.views}</span>
+        </li>
+        <li className="stat-elem">
+          <span className="label">Likes</span>
+          <span className="quantity">{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
